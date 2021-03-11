bdeb6fd2d7932606ccdfdbb522c235c9a5024741
Luis Daniel Reyes <Luis Daniel Reyes@LAPTOP-L56NL5MQ>
2021-03-11 17:36:34 -0600

    Creating new project for lab 2

---
+++ labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Character Creator
+ * ITSE 1430
+ * Spring 2021
+ * Luis Reyes
+ */
+using System;
+
+namespace CharacterCreator.ConsoleHost
+{
+    class Program
+    {
+        static void Main()
+        {
+      
+        }
+    }
+}
