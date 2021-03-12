a859d716c2ba938317454a96e5ef4705521859ef
Luis Daniel Reyes <Luis Daniel Reyes@LAPTOP-L56NL5MQ>
2021-03-11 21:04:24 -0600

    Story 4: unfinished, added new menu's for the profession and race of the character.

--- labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
@@ -35,8 +35,8 @@ namespace CharacterCreator.ConsoleHost
         {
             Console.WriteLine();
             Console.WriteLine("-------------");
-            Console.WriteLine("A)");
-            Console.WriteLine("V)");
+            Console.WriteLine("A)dd a character");
+            Console.WriteLine("V)iew a character");
             Console.WriteLine("Q) uit");
             //Console input
             do
@@ -45,7 +45,7 @@ namespace CharacterCreator.ConsoleHost
                 switch (input)
                 {
                     case "A":
-                    case "a": return 'A';
+                    case "a": AddCharacter(); return 'A';
                     case "Q":
                     case "q": return 'Q';
                     case "V":
@@ -55,11 +55,154 @@ namespace CharacterCreator.ConsoleHost
                 DisplayError("Invalid option");
             } while (true);
         }
+     
+       static void AddCharacter ()
+        {
+            Character character = new Character();
+            Console.Write("Enter Character Name: ");
+            character.Name = Console.ReadLine();
+
+
+            Console.Write("Enter an optional description: ");
+            character.Description = Console.ReadLine();
+
+            if (!character.Validate(out var message))
+            {
+                DisplayError("Character is not valid");
+                return;
+            };
+
+        }
+        /// <summary> Profession options for the user</summary>
+        static void professionOption ()
+        {
+            bool done = false;
+            do
+            {
+                char option = DisplayProfessionMenu();
+                switch (option)
+                {
+                    case 'F':
+                    break;
+                    case 'H':
+                    break;
+                    case 'P':
+                    break;
+                    case 'R':
+                    break;
+                    case 'W':
+                    break;
+                    case 'Q':
+                    done =true;
+                    break;
+                    default: DisplayError("Unknown Command"); break;
+                };
+            } while (!done);
+        }
+
+        /// <summary> Display the character profession menu</summary>
+        private static char DisplayProfessionMenu ()
+        {
+            Console.WriteLine("Choose your character's profession");
+            Console.WriteLine("-------------");
+            Console.WriteLine("F) Fighter");
+            Console.WriteLine("H) Hunter");
+            Console.WriteLine("P) Priest");
+            Console.WriteLine("R) Rogue");
+            Console.WriteLine("W) Wizard");
+            Console.WriteLine("Q) uit");
+            //Console input
+            do
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "F":
+                    case "f": ; return 'F';
+                    case "H":
+                    case "h": return 'H';
+                    case "P":
+                    case "p": return 'P';
+                    case "R":
+                    case "r": return 'R';
+                    case "W":
+                    case "w": return 'W';
+                    case "Q":
+                    case "q": return 'Q';
+                    default: DisplayError("Unknown Command"); break;
+                };
+                DisplayError("Invalid option");
+            } while (true);
+        }
+
+        /// <summary> Race options for the user</summary>
+        static void raceOption ()
+        {
+            bool done = false;
+            do
+            {
+                char option = DisplayRaceMenu();
+                switch (option)
+                {
+                    case 'F':
+                    break;
+                    case 'H':
+                    break;
+                    case 'P':
+                    break;
+                    case 'R':
+                    break;
+                    case 'W':
+                    break;
+                    case 'Q':
+                    done =true;
+                    break;
+                    default: DisplayError("Unknown Command"); break;
+                };
+            } while (!done);
+        }
+
+        /// <summary> Display the character race menu</summary>
+        private static char DisplayRaceMenu ()
+        {
+            Console.WriteLine("Choose your character's race");
+            Console.WriteLine("-------------");
+            Console.WriteLine("D) Dwarf");
+            Console.WriteLine("E) Elf");
+            Console.WriteLine("G) Gnome");
+            Console.WriteLine("T) Half Elf");
+            Console.WriteLine("H) Human");
+            Console.WriteLine("Q) uit");
+            //Console input
+            do
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "D":
+                    case "d":; return 'D';
+                    case "E":
+                    case "e": return 'E';
+                    case "G":
+                    case "g": return 'G';
+                    case "T":
+                    case "t": return 'T';
+                    case "H":
+                    case "h": return 'h';
+                    case "Q":
+                    case "q": return 'Q';
+                    default: DisplayError("Unknown Command"); break;
+                };
+                DisplayError("Invalid option");
+            } while (true);
+        }
+
+
         static void DisplayError ( string message )
         {
             Console.WriteLine(message);
         }
 
-    }//end of program
+    }
    }
 
---
+++ labs/Lab2/CharacterCreator/Character.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CharacterCreator
+{
+    public class Character
+    {
+        public Character ()// defualt construction
+         {
+            //Initialize the fields that cannot be initialized using the field initializer syntax
+            _description = _name;
+         }
+        //Allows you to create the instance and set a common property all at once
+        public Character ( string Name )
+         {
+            _name=Name;
+         }
+
+        /// <summary>Validates the movie data is correct.</summary>
+        /// <param name="error">determines if movie has beeen restored</param>
+        /// <returns>True if movie is valid</returns>
+        public bool Validate ( out string error )
+        {
+            //Name is required
+            if (String.IsNullOrEmpty(Name))
+            {
+                error = "Name is required. ";
+                return false;
+            };
+            error = "";
+            return true;
+        }
+
+
+
+        public string Name
+           {
+            //getter - string Name()
+            get 
+            {
+                //Return Name if not null or empty string otherwise
+                return _name ??""; 
+            }
+            set   
+            {  _name = value?.Trim() ?? "";}
+           }
+         /// <summary> Gets or sets the name. </summary>
+        private string _name = "";
+        public string Description
+            {
+            get { return _description ?? ""; }
+            set { _description = value; }
+            }
+        private string _description = "";
+
+
+    }
+    
+}
