cc2af3e069feba30b50a00505135fac23f3f8491
Luis Daniel Reyes <Luis Daniel Reyes@LAPTOP-L56NL5MQ>
2021-03-11 18:29:39 -0600

    Created a display menu and added Exit option for the user

--- labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
@@ -10,9 +10,56 @@ namespace CharacterCreator.ConsoleHost
 {
     class Program
     {
-        static void Main()
+        static void Main ()
         {
-      
+            bool done = false;
+            do
+            {
+                char option = DisplayMainMenu();
+                switch (option)
+                {
+                    case 'A': 
+                    break;
+                    case 'V':
+                    break;
+                    case 'Q':
+                    done =true;
+                    break;
+                    default: DisplayError("Unknown Command"); break;
+                };
+            } while (!done);
+        }//end of main
+
+        /// <summary> Display the menu</summary>
+        private static char DisplayMainMenu ()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine("A)");
+            Console.WriteLine("V)");
+            Console.WriteLine("Q) uit");
+            //Console input
+            do
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "A":
+                    case "a": return 'A';
+                    case "Q":
+                    case "q": return 'Q';
+                    case "V":
+                    case "v": return 'V';
+                    default: DisplayError("Unknown Command"); break;
+                };
+                DisplayError("Invalid option");
+            } while (true);
         }
-    }
-}
+        static void DisplayError ( string message )
+        {
+            Console.WriteLine(message);
+        }
+
+    }//end of program
+   }
+
