b14904e9f4d7c84978a41abc9d619d09a835e0c7
Luis Daniel Reyes <Luis Daniel Reyes@LAPTOP-L56NL5MQ>
2021-03-22 18:32:41 -0500

    Add file,edit, delete, and exit to the designer

--- classwork/MovieLibrary/MovieLibrary.Winhost/MainForm.cs
+++ classwork/MovieLibrary/MovieLibrary.Winhost/MainForm.cs
@@ -10,5 +10,38 @@ namespace MovieLibrary.Winhost
             InitializeComponent();
         }
 
+        private void MainForm_Load ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void mainMenu_ItemClicked ( object sender, ToolStripItemClickedEventArgs e )
+        {
+
+        }
+
+        private void OnHelpAbout ( object sender, EventArgs e )
+        {
+            // MessageBox.Show("Help - About");
+            var form = new AboutBox();
+
+            //show the form modally
+            form.ShowDialog();
+            //form.Show(); //modeless
+
+
+
+        }
+
+        private void OnFileExit ( object sender, EventArgs e )
+        {
+            //display a conformation and quit if yes
+            var result = MessageBox.Show(this,"Are you sure if you want to exit)", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result != DialogResult.Yes)
+                return;
+            //Close the form
+            Close();
+
+        }
     }
 }
