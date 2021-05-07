57a9161ce7eaf7f3f33cba06d1f1b227035c0719
Luis Daniel Reyes <Luis Daniel Reyes@LAPTOP-L56NL5MQ>
2021-05-07 14:25:26 -0500

    check the commit all

--- labs/Lab5/CharacterCreator/Memory/MemoryCharacterRoster.cs
+++ labs/Lab5/CharacterCreator/Memory/MemoryCharacterRoster.cs
@@ -3,6 +3,7 @@
 //Character Collections
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
